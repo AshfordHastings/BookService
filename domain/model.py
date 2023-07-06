cbed39511533d2ee5b19f0ce02eaae44afdc97ef
Ashford Hastings <ashfordhastings@Ashfords-MBP.attlocal.net>
2023-07-05 19:02:15 -0500

    domain/model

--- domain/model.py
+++ domain/model.py
@@ -1,31 +1,83 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from typing import Dict, List, Tuple
 from pathlib import Path
-from uuid import UUID, uuid4
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-AuthorID = UUID
-BookID = UUID
 
-@dataclass
-class Author:
-    first_name: str
-    last_name: str
-    id: AuthorID = uuid4()
-@dataclass 
-class BookMetadata:
-    file_extension: str
-@dataclass
-class BookInfo:
-    title: str
-    year: int
-    author: Author
+class Base(DeclarativeBase):
+    pass
 
-class Book:
-    def __init__(self, info:BookInfo, metadata: BookMetadata, id:BookID=uuid4()):
-        self.id = id
+
+class BookMetadata(Base):
+    __tablename__ = "metadata"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    file_extension: Mapped[str]
+
+    def __init__(self, file_extension):
+        self.file_extension = file_extension
+
+class Author(Base):
+    __tablename__ = "author"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str]
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+class BookInfo(Base):
+    __tablename__ = "info"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    year: Mapped[int]
+    author_id: Mapped[int] = mapped_column(ForeignKey("author.id"))
+
+    author: Mapped['Author'] =  relationship()
+
+    def __init__(self, title, year, author):
+        self.title = title
+        self.year = year
+        self.author = author
+
+class Book(Base):
+    __tablename__ = "book"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    info_id: Mapped[int] = mapped_column(ForeignKey("info.id"))
+    metadata_id: Mapped[int] = mapped_column(ForeignKey("metadata.id"))
+
+    book_metadata: Mapped['BookMetadata'] = relationship()
+    info: Mapped['BookInfo'] = relationship()
+
+    def __init__(self, info, book_metadata):
         self.info = info
-        self.book = metadata
+        self.book_metadata = book_metadata
+
+
+
+
+
+
+# @dataclass
+# class Author:
+#     first_name: str
+#     last_name: str
+#     id: AuthorID = uuid4()
+# @dataclass 
+# class BookMetadata:
+#     file_extension: str
+# @dataclass
+# class BookInfo:
+#     title: str
+#     year: int
+#     author: Author
+
+# class Book:
+#     def __init__(self, info:BookInfo, metadata: BookMetadata, id:BookID=uuid4()):
+#         self.id = id
+#         self.info = info
+#         self.book = metadata
 
 class Shelf:
     def __init__(self, host:'Host', books:Dict[str, Book]={}):
